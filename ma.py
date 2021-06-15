e19238fc60a979558040c280e8b06e7317f4d683
Amanuel Debebe <amanueldebebe1000@gmail.com>
2021-06-15 19:34:07 +0300

    Fix CourseNameSpace routes

--- ma.py
+++ ma.py
@@ -1,18 +1,21 @@
 from flask_marshmallow import Marshmallow
+from flask_restplus import fields
+from flask_restplus.inputs import email
 from flask_sqlalchemy import model
-from .models import *
+from models import *
 
 ma = Marshmallow()
 
 
 class StudentSchema(ma.Schema):
     class Meta:
+        order =True
         fields = ("FirstName","LastName","Email", "StudentID")
         model = Students
 
 class InstructorSchema(ma.Schema):
     class Meta:
-        fields = ("FirstName","LastName","Email")
+        fields = ("InstructorID","FirstName","LastName","Email")
         model = Instructors
 
 # class ClassroomSchema(ma.Schema):
@@ -22,10 +25,21 @@ class InstructorSchema(ma.Schema):
 
 class CourseSchema(ma.Schema):
     class Meta:
-
         fields = ("CourseID","InstructorID","CourseTitle","CourseDescription")
         model = Courses
-        model = Instructors
-# class Resource(ma.Schema):
-#     class Meta:
-#         fields = ("FirstName","LastName","Email")
+
+  
+
+
+class ADDSTUDENT:
+    CourseID=0
+    email= ""
+    name= ""
+
+class AddStudentSchema(ma.Schema):
+    class Meta:
+        fields= ("CourseID","email","name")
+        model = ADDSTUDENT
+    
+
+
