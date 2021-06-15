a082288e1b070048008f5e2942c4701d6fe5927c
Hanan Miftah <se.hanan.miftah@gmail.com>
2021-06-15 19:25:46 +0300

    commit

--- ma.py
+++ ma.py
@@ -1,5 +1,4 @@
 from flask_marshmallow import Marshmallow
-from flask_sqlalchemy import model
 from .models import *
 
 ma = Marshmallow()
@@ -7,25 +6,6 @@ ma = Marshmallow()
 
 class StudentSchema(ma.Schema):
     class Meta:
-        fields = ("FirstName","LastName","Email", "StudentID")
-        model = Students
-
-class InstructorSchema(ma.Schema):
-    class Meta:
         fields = ("FirstName","LastName","Email")
-        model = Instructors
+        # model = Students
 
-# class ClassroomSchema(ma.Schema):
-#     class Meta:
-#         model = ClassroomStudents()
-#         fields = ("FirstName","LastName","Email")
-
-class CourseSchema(ma.Schema):
-    class Meta:
-
-        fields = ("CourseID","InstructorID","CourseTitle","CourseDescription")
-        model = Courses
-        model = Instructors
-# class Resource(ma.Schema):
-#     class Meta:
-#         fields = ("FirstName","LastName","Email")
