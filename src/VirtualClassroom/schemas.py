34baa65a3c986f0b1edeffe91f1f1190ba1f0179
Abdi <se.abdulfeta.dedgba@gmail.com>
2021-06-15 22:15:44 +0300

    add some validation

--- src/VirtualClassroom/schemas.py
+++ src/VirtualClassroom/schemas.py
@@ -1,4 +1,5 @@
-from flask_marshmallow import Marshmallow
+from marshmallow import fields, Schema, validate, validates, ValidationError
+from models import *
 from flask_sqlalchemy import model
 from . import ma
 
@@ -9,12 +10,37 @@ class StudentSchema(ma.Schema):
     class Meta:
         fields = ("FirstName","LastName","Email", "StudentID")
         model = Students
+        ordered=True
+    StudentID=fields.Integer(data_key="userid")
+    FirstName = fields.String(required=True)
+    LastName = fields.String(required=True)
+    Email = fields.Email(required=True)
+    
+    @validates("Email")
+    def validate_username(self, username):
+        if bool(Students.query.filter_by(Email=Email).first()):
+            raise ValidationError(
+                '"{username}" Email already exists, '
+                'please use a different username.'.format(Email=Email)
+            )
+
 
 class InstructorSchema(ma.Schema):
     class Meta:
         fields = ("FirstName","LastName","Email")
         model = Instructors
-
+    InstructorID=fields.Integer(data_key="userid")
+    FirstName = fields.String(required=True)
+    LastName = fields.String(required=True)
+    Email = fields.Email(required=True)
+    
+    @validates("Email")
+    def validate_username(self, username):
+        if bool(Instructors.query.filter_by(Email=Email).first()):
+            raise ValidationError(
+                '"{username}" Email already exists, '
+                'please use a different username.'.format(Email=Email)
+            )
 # class ClassroomSchema(ma.Schema):
 #     class Meta:
 #         model = ClassroomStudents()
