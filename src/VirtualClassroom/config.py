942c4698390ee0cbba477714d7ffb61ee9ccbdba
Infinidrix <biruksolomon11@gmail.com>
2021-06-17 07:16:28 +0300

    update config for heroku

--- src/VirtualClassroom/config.py
+++ src/VirtualClassroom/config.py
@@ -1,8 +1,10 @@
+import os
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://xgkqkevc:64RJl4BfrwdyN7R_fhCI6ZdKERgfLFSY@batyr.db.elephantsql.com/xgkqkevc'
+
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost/virtualclassdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    JWT_SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+    SECRET_KEY = os.getenv(SECRET_KEY)
+    JWT_SECRET_KEY = os.getenv(JWT_SECRET_KEY)
     JWT_BLACKLIST_ENABLED = ['access']
     UPLOAD_FOLDER = './../../static'
