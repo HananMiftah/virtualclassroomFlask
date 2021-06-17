d65ce5691985d4949d4ac5c1a189219216fd8078
Infinidrix <biruksolomon11@gmail.com>
2021-06-17 07:52:26 +0300

    fix bug with env vars

--- src/VirtualClassroom/config.py
+++ src/VirtualClassroom/config.py
@@ -4,7 +4,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root@localhost/virtualclassdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv(SECRET_KEY)
-    JWT_SECRET_KEY = os.getenv(JWT_SECRET_KEY)
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     JWT_BLACKLIST_ENABLED = ['access']
     UPLOAD_FOLDER = './../../static'
