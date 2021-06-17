dea61f248d3be59f4d666ade84a8eca09f52383a
Infinidrix <biruksolomon11@gmail.com>
2021-06-17 05:41:04 +0300

    add student

--- src/VirtualClassroom/Resources/student.py
+++ src/VirtualClassroom/Resources/student.py
@@ -36,3 +36,52 @@ class Student(Resource):
         db.session.add(new_student)
         db.session.commit()
         return student_schema.dump(new_student), 201
+
+
+@StudentNamespace.route('/<int:stuID>')
+class studentResource(Resource):
+    @jwt_required()
+    def get(self,stuID):
+        '''
+        Get Student Info
+        '''
+        student = Students.query.filter_by(StudentID=stuID).first()
+
+        if student:
+            return student_schema.dump(student)
+        return "Student not found",404
+    @api.expect(student)
+    @jwt_required()
+    def patch(self,studentId):
+        '''
+        Edit Student Info
+        '''
+        student = Students.query.filter_by(StudentID=studentId).first()
+
+        #updating required fields
+        for key in request.json.keys():
+            if key == 'FirstName':
+                student.FirstName = request.json[key]
+            elif key == 'LastName':
+                student.LastName = request.json[key]
+            elif key == 'Email':
+                student.Email = request.json[key]
+        db.session.commit()
+
+        return student_schema.dump(student), 200
+
+
+@StudentNamespace.route('/studentByEmail/<string:email>')
+class StudentByEmail(Resource):
+    @jwt_required()
+    def get(self,email):
+        '''
+        Get Student Info
+        '''
+        student = Students.query.filter_by(Email=email).first()
+        print("SFNAME")
+        print(student.FirstName)
+        print("SFNAME")
+        if student:
+            return student_schema.dump(student)
+        return abort(404, "Student not found")
